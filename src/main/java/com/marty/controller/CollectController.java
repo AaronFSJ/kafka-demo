3a4a93dcce2120a5a71e6609427a128308890654
Aaron <417186152@qq.com>
2017-12-29 11:38:58 +0800

    添加说明

--- src/main/java/com/marty/controller/CollectController.java
+++ src/main/java/com/marty/controller/CollectController.java
@@ -39,7 +39,13 @@ public class CollectController {
             User user = new User("1","Aaron",30,new BigDecimal(("20000")));
             //将对象转成byte数组
             byte[] u= BeanUtil.ObjectToBytes(user);
-            ProducerRecord<String,Object> pr = new ProducerRecord<String,Object>("user",0,"user",u);
+            /**
+             * spring-kafka,可以实现producer的消息均发，但是如果producer中没有定义分区策略，也就是说程序采用默认的kafka.producer.DefaultPartitioner
+             * 其核心思想就是对每个消息的key的hash值对partition数取模得到，所以如果key没有传入，则key为null,代码每次的hash值都一样，因此只发到了一个分区上，
+             * 所以如果想要分送到不同的分区上，可以自己实现Partitioner，也可以指定每条消息发送的key,key值要根据实际情况发生变化，虽然不是均发，
+             * 但是至少能保证不是每个消息都发送到同一个分区上
+             */
+            ProducerRecord<String,Object> pr = new ProducerRecord<String,Object>("user","user",u);
             for(int i =0;i<2;i++){
                 kafkaTemplate.send(pr);
                 logger.info("已经发送一个user");
